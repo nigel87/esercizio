74218ff35e91ab308f81851bcb8fa15b91ad5cca
nigel87 <nigelpllaha@gmail.com>
2023-04-12 09:08:42 +0200

    Modifica messaggio d'errore per usare la request generica

--- src/main/java/it/nigelpllaha/esercizio/constants/ErrorMessages.java
+++ src/main/java/it/nigelpllaha/esercizio/constants/ErrorMessages.java
@@ -4,6 +4,10 @@ public class ErrorMessages {
     public static final String NULL_FABRICK_RESPONSE = "Impossibile ottenere i dati richiesti, fabrickResponse è null ";
     public static final String HTTP_ERROR = "Impossibile procedere, errore HTTP: ";
     public static final String REST_CLIENT_EXCEPTION = "Impossibile procedere, RestClientException ";
+    public static final String VALIDATION_ERROR = "È necessario specificare un valore correto per i campi obbligatori";
+    public static final String DATE_ERROR = "Inserire una data valida";
+
+    public static final String FABRICK_API_ERROR = "Errore durante la chimata al API di Fabrick";
 
     public static final String PARSING_ERROR = "Impossibile parsare l'errore: ";
     public static final String FIELD_REQUIRED = "Il campo è obbligatorio";
--- src/main/java/it/nigelpllaha/esercizio/exception/GlobalExceptionHandler.java
+++ src/main/java/it/nigelpllaha/esercizio/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package it.nigelpllaha.esercizio.exception;
 
 import it.nigelpllaha.esercizio.dto.ApiErrorResponse;
+import it.nigelpllaha.esercizio.dto.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -10,42 +11,36 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static it.nigelpllaha.esercizio.constants.ErrorMessages.*;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(FabrickApiException.class)
-    public ResponseEntity<ApiErrorResponse> handleFabrickApiException(FabrickApiException e) {
+    public ResponseEntity<Response<String>> handleFabrickApiException(FabrickApiException e) {
         int httpStatusCode = e.getStatusCode().value();
-
-        ApiErrorResponse errorResponse = new ApiErrorResponse();
-         errorResponse.setMessage(e.getMessage());
-        if(e.getErrorMessages()!=null && !e.getErrorMessages().isEmpty()) {
-            errorResponse.setErrors(e.getErrorMessages());
-        }
-        return ResponseEntity.status(httpStatusCode).body(errorResponse);
+        Response<String> response = new Response<>(false,e.getMessage(),FABRICK_API_ERROR);
+        return ResponseEntity.status(httpStatusCode).body(response);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<Response<List<String>>> handleValidationException(MethodArgumentNotValidException ex) {
         BindingResult result = ex.getBindingResult();
         List<FieldError> fieldErrors = result.getFieldErrors();
 
-        ApiErrorResponse errorResponse = new ApiErrorResponse();
-        errorResponse.setMessage("Validation failed");
-        List<String> errors = fieldErrors.stream()
+         List<String> errors = fieldErrors.stream()
                 .map(fieldError -> fieldError.getField() + ":" + fieldError.getDefaultMessage())
                 .toList();
-        errorResponse.setErrors(errors);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+
+        Response<List<String>> response = new Response<>(false,errors,VALIDATION_ERROR);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     @ExceptionHandler(InvalidAccountingDatesException.class)
-    public ResponseEntity<ApiErrorResponse> handleInvalidAccountingDatesException(InvalidAccountingDatesException ex) {
-
-        ApiErrorResponse errorResponse = new ApiErrorResponse();
-        errorResponse.setMessage("Validation failed for AccountingDates");
-        errorResponse.setErrors(ex.getErrorMessages());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    public ResponseEntity<Response<List<String>>> handleInvalidAccountingDatesException(InvalidAccountingDatesException ex) {
+        Response<List<String>> response = new Response<>(false,ex.getErrorMessages(), DATE_ERROR);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
 
